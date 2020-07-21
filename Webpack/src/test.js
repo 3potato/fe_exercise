4485932b65b125ed7beb58185ad982c6e9ddf24c
fx <fengxue1@guazi.com>
2020-07-21 19:05:46 +0800

    feat: webpack学习

---
+++ Webpack/src/test.js
@@ -0,0 +1,13 @@
+import _ from 'lodash';
+
+  function component() {
+    var element = document.createElement('span');
+
+
+   // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+  }
+
+  document.body.appendChild(component());
